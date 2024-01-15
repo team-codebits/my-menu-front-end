3f68bcd1fe28844e09d07272038b028713818b52
Érick Santos <ericksantos@mailfence.com>
2024-01-15 20:13:21 -0300

    adds new header from menubar

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import PrimeVue from 'primevue/config';
+import "./assets/main.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-light-green/theme.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
-app.mount('#app')
+app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,142 +1,55 @@
 <script setup>
 import Menubar from "primevue/menubar";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const items = ref([
   {
     label: "Home",
-    icon: "pi pi-home",
+    route: "/",
   },
   {
-    label: "Features",
-    icon: "pi pi-star",
+    label: "Sobre",
+    route: "/about",
   },
   {
-    label: "Projects",
-    icon: "pi pi-search",
-    items: [
-      {
-        label: "Core",
-        icon: "pi pi-bolt",
-        shortcut: "⌘+S",
-      },
-      {
-        label: "Blocks",
-        icon: "pi pi-server",
-        shortcut: "⌘+B",
-      },
-      {
-        label: "UI Kit",
-        icon: "pi pi-pencil",
-        shortcut: "⌘+U",
-      },
-      {
-        separator: true,
-      },
-      {
-        label: "Templates",
-        icon: "pi pi-palette",
-        items: [
-          {
-            label: "Apollo",
-            icon: "pi pi-palette",
-            badge: 2,
-          },
-          {
-            label: "Ultima",
-            icon: "pi pi-palette",
-            badge: 3,
-          },
-        ],
-      },
-    ],
+    label: "Login",
+    route: "/login",
   },
   {
-    label: "Contact",
-    icon: "pi pi-envelope",
-    badge: 3,
+    label: "SignUp",
+    route: "/signup",
+  },
+  {
+    label: "Dashboard",
+    route: "/dashboard",
   },
 ]);
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/signup">Criar Conta</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="card">
+    <Menubar :model="items">
+      <template #item="{ item, props, hasSubmenu }">
+        <router-link
+          v-if="item.route"
+          v-slot="{ href, navigate }"
+          :to="item.route"
+          custom
+        >
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+      </template>
+    </Menubar>
+  </div>
 
   <RouterView />
   <main></main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
