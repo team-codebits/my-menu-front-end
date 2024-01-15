a2195e7f8dd34a1424381d2a1751a716a05fd5cf
Érick Santos <ericksantos@mailfence.com>
2024-01-15 20:00:16 -0300

    adds template from menubar prime vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,63 @@
-<script setup></script>
+<script setup>
+import Menubar from "primevue/menubar";
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+  },
+  {
+    label: "Features",
+    icon: "pi pi-star",
+  },
+  {
+    label: "Projects",
+    icon: "pi pi-search",
+    items: [
+      {
+        label: "Core",
+        icon: "pi pi-bolt",
+        shortcut: "⌘+S",
+      },
+      {
+        label: "Blocks",
+        icon: "pi pi-server",
+        shortcut: "⌘+B",
+      },
+      {
+        label: "UI Kit",
+        icon: "pi pi-pencil",
+        shortcut: "⌘+U",
+      },
+      {
+        separator: true,
+      },
+      {
+        label: "Templates",
+        icon: "pi pi-palette",
+        items: [
+          {
+            label: "Apollo",
+            icon: "pi pi-palette",
+            badge: 2,
+          },
+          {
+            label: "Ultima",
+            icon: "pi pi-palette",
+            badge: 3,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Contact",
+    icon: "pi pi-envelope",
+    badge: 3,
+  },
+]);
+</script>
 
 <template>
   <header>
