82743b0f782b439c3589f13e77c84323f2d20f89
Érick Santos <ericksantos@mailfence.com>
2024-01-15 19:57:01 -0300

    adds dashboard to router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
 import SignUpView from "../views/SignUpView.vue";
+import DashboardViewVue from "@/views/DashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: "/signup",
       name: "signup",
       component: SignUpView,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: DashboardViewVue,
     },
   ],
 });
